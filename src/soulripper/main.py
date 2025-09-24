c703b25c4fa47b3ae5ae34c56835a2fa3b5d566a
jwapico <altizer16@outlook.com>
2025-09-23 19:21:24 -0600

    Refactor project to not run in docker

--- src/soulripper/main.py
+++ src/soulripper/main.py
@@ -1,11 +1,12 @@
 import asyncio
 import sys
+import os
 
 from soulripper.utils import AppParams, extract_app_params, init_logger
 from soulripper.cli import CLIOrchestrator
 
 async def main():
-    app_params: AppParams = extract_app_params("/home/soulripper/config.yaml")
+    app_params: AppParams = extract_app_params(f"{os.getcwd()}/config.yaml")
     
     init_logger(app_params.log_filepath, app_params.log_level, app_params.db_echo)
 
