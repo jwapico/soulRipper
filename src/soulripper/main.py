274f6388c7e80201ddc840c5e356138d1c5e70b8
jwapico <altizer16@outlook.com>
2026-03-20 23:32:59 -0600

    fix bad things

--- src/soulripper/main.py
+++ src/soulripper/main.py
@@ -6,7 +6,7 @@ from soulripper.utils import AppParams, extract_app_params, init_logger
 from soulripper.cli import CLIOrchestrator
 
 async def main():
-    app_params: AppParams = extract_app_params(f"{os.getcwd()}/config.yaml")
+    app_params: AppParams = extract_app_params(f"{os.getcwd().replace('src', '')}/config.yaml")
     
     init_logger(app_params.log_filepath, app_params.log_level, app_params.db_echo)
 
@@ -14,8 +14,5 @@ async def main():
         cli_orchestrator = CLIOrchestrator(app_params)
         await cli_orchestrator.run()
 
-def soulrip():
-    asyncio.run(main())
-
 if __name__ == "__main__":
-    soulrip()
+    asyncio.run(main())
