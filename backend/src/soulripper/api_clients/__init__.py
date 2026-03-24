ffbb35ee22457aae28a74d81452185b185b71c68
jwapico <altizer16@outlook.com>
2026-03-23 23:04:41 -0600

    initialize svelte frontend !

--- backend/src/soulripper/api_clients/__init__.py
+++ backend/src/soulripper/api_clients/__init__.py
@@ -1,7 +1,5 @@
-from .discogs_client import DiscogsClient
 from .spotify_client import SpotifyUserData, SpotifyClient
 
 __all__ = [
-    "DiscogsClient",
     "SpotifyUserData", "SpotifyClient"
 ]
--- src/soulripper/api_clients/discogs_client.py
+++
@@ -1,5 +0,0 @@
-import discogs_client
-
-class DiscogsClient:
-    def __init__(self, user_token: str):
-        self._client = discogs_client.Client("soulripper/0.1", user_token=user_token)
